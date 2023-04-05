acc3a1e891548aa274c1478b8b6e3b0ffdd87716
Anton Kytmanov <antonkytm@mail.ru>
2023-04-05 20:33:49 +0500

    Ready 1 task

---
+++ src/Basket.java
@@ -0,0 +1,94 @@
+import java.io.*;
+
+public class Basket {
+    protected static int[] prices;
+    protected static String[] products;
+    protected static int[] totalSum;
+
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        totalSum = new int[products.length];
+    }
+
+    public Basket(String[] products, int[] prices, int[] totalSum) {
+        this.products = products;
+        this.prices = prices;
+        this.totalSum = totalSum;
+    }
+
+    public void addToCart(int productNum, int amount) {
+        if ((0 < productNum) && (productNum < products.length + 1)) {
+            totalSum[productNum - 1] += amount * prices[productNum - 1];
+        } else {
+            System.out.println("Введите корректный номер товара!");
+        }
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + ". " + products[i] + " " + totalSum[i] + " руб.");
+        }
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        OutputStream text = new FileOutputStream(textFile);
+        for (int i = 0; i < products.length; i++) {
+            text.write((products[i] + " " + totalSum[i]).getBytes());
+            text.write("\n".getBytes());
+        }
+    }
+
+    public Basket loadFromTxtFile(File textFile) throws IOException {
+        try {
+
+            BufferedReader reader = new BufferedReader(new FileReader(textFile));
+            LineNumberReader howLines = new LineNumberReader(new FileReader(textFile));
+
+            int lines = 0;
+            while (howLines.readLine() != null) {
+                lines++;
+            }
+
+            howLines.close();
+            String[] loadProducts = new String[lines];
+            int[] loadPrices = new int[lines];
+            int i = 0;
+
+            String line = reader.readLine();
+            while (i < lines) {
+
+                String[] parts = line.split(" ");
+                String nameProducts = parts[0];
+                int productCount = Integer.parseInt(parts[1]);
+
+                loadProducts[i] = nameProducts;
+                totalSum[i] = productCount;
+
+                if (productCount != 0) {
+                    loadPrices[i] = productCount / (productCount / prices[i]);
+                } else {
+                    loadPrices[i] = prices[i];
+                }
+
+                line = reader.readLine();
+                i++;
+            }
+
+            reader.close();
+            Basket loadBasket = new Basket(loadProducts, loadPrices, totalSum);
+            System.out.println("Ваша корзина найдена, данные восстановлены.");
+            loadBasket.printCart();
+            return loadBasket;
+
+        } catch (FileNotFoundException e) {
+        }
+        return null;
+    }
+
+
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,45 @@
-import java.util.Arrays;
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int sumProduction = 0;
-        String[] products = {"Молоко", "Хлеб", "Гречневая крупа", "Яблоко", "Баранина"};
-        int[] prices = {50, 14, 80, 5, 1050};
+    public static void main(String[] args) throws Exception{
 
+        File textFile = new File("File.txt");
+        Scanner scanner = new Scanner(System.in);
+
+        String[] products = {"Молоко", "Хлеб", "Гречка", "Яблоко", "Баранина"};
+        int[] prices = {50, 14, 80, 5, 1050};
         System.out.println("Список возможных товаров для покупки:");
         for (int i = 0; i < products.length; i++) {
-            System.out.println( (i+1) + ". "+ products[i] + " " + prices[i] + " руб./шт.");
-            }
-
-        int[] totalSum = new int[products.length];
-        int[] totalCount = new int[products.length];
-        for (int i = 0; i < products.length; i++) {
-            totalSum[i] = 0;
-            totalCount[i] = 0;
+            System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб.");
         }
 
-        while (true) {
+        Basket basket = new Basket(products, prices);
+        if (!textFile.exists()){
+            System.out.println("Корзина не найдены, будет составлена заново");
+        }
+        basket.loadFromTxtFile(textFile);
 
-            System.out.println("Введите номер товара и его количество или введите 'end' для завершения программы");
+        while (true) {
+            System.out.println("Введите номер товара и его количество, 'basket' для вывода вашей корзины, 'end' для завершения программы");
             String input = scanner.nextLine();
             if (input.equals("end")) {
+                basket.printCart();
+                basket.saveTxt(textFile);
                 break;
             }
+            if (input.equals("basket")){
+                basket.printCart();
+                continue;
+            }
+
             String[] parts = input.split(" ");
             int productNumber = Integer.parseInt(parts[0]);
             int productCount = Integer.parseInt(parts[1]);
-            if ((0 < productNumber) && (productNumber < products.length + 1)) {
-                totalSum[productNumber - 1] += productCount * prices[productNumber - 1];
-                totalCount[productNumber - 1] += productCount;
-            } else {
-                System.out.println("Введите корректный номер товара!");
-                continue;
-            }
-        }
+            basket.addToCart(productNumber, productCount);
 
-        System.out.println("Ваша корзина:\n");
-        for (int i = 0; i < products.length; i++) {
-            if (totalSum[i] > 0) {                                                          
-                System.out.println(products[i] + " " + totalCount[i] + "шт. " + prices[i] + " руб./шт. " + totalSum[i] + " руб. в сумме.\n");
-            } else {
-                continue;
-            }
         }
     }
 }
